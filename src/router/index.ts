a1959f5ede3846eeb0edafc2a5875f198446155c
AC-0308 <pzzxtyh@qq.com>
2026-03-20 11:38:04 +0800

    清空所有页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,26 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'software-list',
-      component: () => import('@/views/SoftwareListView.vue'),
-    },
-    {
-      path: '/detail/:name',
-      name: 'software-detail',
-      component: () => import('@/views/SoftwareDetailView.vue'),
-    },
-    {
-      path: '/policy-rules',
-      name: 'policy-rules',
-      component: () => import('@/views/PolicyRulesView.vue'),
-    },
-    {
-      path: '/test-form1',
-      name: 'test-form1',
-      component: () => import('@/views/TestForm1View.vue'),
-    },
+
   ],
 })
 
